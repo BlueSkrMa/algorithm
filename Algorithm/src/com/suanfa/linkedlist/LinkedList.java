a5c9fc533cc36ea005c6771d69094cffd49d479b
blueSkrMa <842670735@qq.com>
2019-06-19 19:48:18 +0800

    s

---
+++ Algorithm/src/com/suanfa/linkedlist/LinkedList.java
@@ -0,0 +1,40 @@
+package com.suanfa.linkedlist;
+
+import java.util.ArrayList;
+
+public class LinkedList {
+	private Node head;
+	//增
+	public void add(int num) {
+		if(head==null) {
+			head=new Node();
+			head.value=num;
+			head.next=new Node();
+			System.out.println("插入成功！");
+			return;
+		}
+		Node temp=head.next;
+		while(temp.next!=null) {
+			temp.value=num;
+			temp=temp.next;
+		}
+		temp.next=new Node();
+	}
+	//查
+	public void show() {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		if(head!=null) {
+			list.add(head.value);
+			Node temp=head.next;
+			while(temp.next!=null) {
+				list.add(temp.value);
+				temp=temp.next;
+			}
+		}
+		System.out.println(list);
+	}
+}
+class Node{
+	public int value;
+	public Node next;
+}
---
+++ Algorithm/src/com/suanfa/test/Test.java
@@ -0,0 +1,14 @@
+package com.suanfa.test;
+
+import com.suanfa.linkedlist.LinkedList;
+
+public class Test {
+	public static void main(String[] args) {
+		LinkedList list = new LinkedList();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.show();
+	}
+}
