d75cfa49196be246f6746425cd7bdb8fcd0cf0f7
blueSkrMa <842670735@qq.com>
2019-06-21 15:10:21 +0800

    a

--- Algorithm/src/com/suanfa/linkedlist/LinkedList.java
+++ Algorithm/src/com/suanfa/linkedlist/LinkedList.java
@@ -2,8 +2,15 @@ package com.suanfa.linkedlist;
 
 import java.util.ArrayList;
 
+import com.suanfa.test.Node;
+
 public class LinkedList {
-	private Node head;
+	public Node head;
+	
+	//返回node，用于其他操作
+	public Node returnNode() {
+		return this.head;
+	}
 	//向末尾插入
 	public void addToLast(int num) {
 		Node node=new Node();
@@ -47,8 +54,47 @@ public class LinkedList {
 		}
 		head=preNode;
 	}
+	//两个有序链表合并 1-3-4-7  2-3-5-6
+	public static Node mergeNode(Node n1,Node n2) {
+		Node cur1=n1;
+		Node cur2=n2;
+		Node node_ = new Node();
+		Node node = node_;
+		while(cur1!=null&&cur2!=null) {
+			if(cur1.value<cur2.value) {
+				node.next=cur1;
+				cur1=cur1.next;
+			}else {
+				node.next=cur2;
+				cur2=cur2.next;
+			}
+			node=node.next;
+			
+		}
+		if(cur1!=null) {
+			node.next=cur1;
+		}
+		if(cur2!=null) {
+			node.next=cur2;
+		}
+		return node_.next;
+	}
+	//链表中环的检测
+	public boolean isExsist(Node node) {
+		if(node==null) {
+			return false;
+		}
+		Node cuNode=node;
+		ArrayList<Node> list = new ArrayList<Node>();
+		while(cuNode!=null) {
+			if(list.contains(cuNode)) {
+				return true;
+			}
+			list.add(cuNode);
+			cuNode=cuNode.next;
+		}
+		return false;
+	}
+	
 }
-class Node{
-	public int value;
-	public Node next;
-}
+
--- Algorithm/src/com/suanfa/test/Test.java
+++ Algorithm/src/com/suanfa/test/Test.java
@@ -1,16 +1,45 @@
 package com.suanfa.test;
 
 import com.suanfa.linkedlist.LinkedList;
-
+import com.suanfa.test.Node;
 public class Test {
 	public static void main(String[] args) {
-		LinkedList list = new LinkedList();
-		list.addToLast(1);
-		list.addToLast(2);
-		list.addToLast(3);
-		list.addToLast(4);
-		list.showAll();
-		list.fanZhaun();
-		list.showAll();
+//		LinkedList list = new LinkedList();
+//		list.addToLast(1);
+//		list.addToLast(3);
+//		list.addToLast(5);
+//		list.addToLast(9);
+//		list.showAll();
+//		Node n1 = list.returnNode();
+//		System.out.println("------");
+//		LinkedList list2 = new LinkedList();
+//		list2.addToLast(2);
+//		list2.addToLast(4);
+//		list2.addToLast(5);
+//		list2.addToLast(11);
+//		list2.showAll();
+//		Node n2 = list2.returnNode();
+//		System.out.println("-------");
+//		Node node = LinkedList.mergeNode(n1, n2);
+//		LinkedList list3 = new LinkedList();
+//		list3.head=node;
+//		list3.showAll();
+		Node a1 = new Node();
+		Node a2 = new Node();
+		Node a3 = new Node();
+		Node a4 = new Node();
+		Node a5 = new Node();
+		a1.value=1;
+		a1.next=a2;
+		a2.value=2;
+		a2.next=a3;
+		a3.value=3;
+		a3.next=a4;
+		a4.value=4;
+		a4.next=a5;
+		a5.value=5;
+		a5.next=a2;
+		boolean exsist = new LinkedList().isExsist(a1);
+		System.out.println(exsist);
 	}
 }
