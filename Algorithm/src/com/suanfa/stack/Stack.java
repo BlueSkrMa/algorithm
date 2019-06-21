4b5db13038c5e14eb7693879b0c952669029cfc5
blueSkrMa <842670735@qq.com>
2019-06-21 17:52:17 +0800

    a

--- Algorithm/src/com/suanfa/stack/Stack.java
+++ Algorithm/src/com/suanfa/stack/Stack.java
@@ -1,5 +1,32 @@
 package com.suanfa.stack;
 
-public class Stack {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Stack {
+	private List<Integer> list;
+	public Stack() {
+		list=new ArrayList<Integer>();
+	}
+	
+	public void push(int num) {
+		list.add(num);
+	}
+	public int pop() {
+		if(list.size()>0) {
+			return list.remove(list.size()-1);
+		}
+		throw new OutOfMemoryError();
+	}
+	public int getMin() {
+		if(list.size()<=0) {
+			throw new OutOfMemoryError();
+		}
+		for (int i = 0; i < array.length; i++) {
+			
+		}
+	}
+	public boolean deleteTop() {
+		
+	}
 }
